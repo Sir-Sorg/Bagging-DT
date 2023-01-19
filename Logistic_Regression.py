8e83cd1bae1113c7da8cf54af37e530c3eb20bd0
Sir-Sorg <sinaorojlo53@gmail.com>
2023-01-19 16:37:37 +0330

    standardization in LG add

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -1,6 +1,6 @@
 import csv
 import numpy as np
-
+from sklearn.linear_model import LogisticRegression
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
@@ -26,6 +26,14 @@ def dummyVariables(features):
         features[:, column] = tranasformer.transform(features[:, column])
     return features
 
+
+def standardization(data):
+    scaler = preprocessing.StandardScaler()
+    scaler.fit(data)
+    STD_data = scaler.transform(data)
+    return STD_data
+
+
 # Read Data
 fileAddress = './train+dev+test.csv'
 data = readData(fileAddress)
@@ -37,8 +45,10 @@ X = data[:, 1:]
 Y = data[:, 0]
 X = dummyVariables(X)
 
+X = standardization(X)
+
 # split train and test subset I use 70% & 30%
 X_train, X_test, y_train, y_test = train_test_split(
     X, Y, test_size=0.3, random_state=24)
-
-
+print('Train set:', X_train.shape,  y_train.shape)
+print('Test set:', X_test.shape,  y_test.shape)
