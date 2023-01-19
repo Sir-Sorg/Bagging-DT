dac7b4e65ba837e406645d162e4febdf5f40958e
Sir-Sorg <sinaorojlo53@gmail.com>
2023-01-19 12:16:18 +0330

    reading data in LG

---
+++ Logistic_Regression.py
@@ -0,0 +1,44 @@
+import csv
+import numpy as np
+
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+
+def readData(address):
+    with open(address) as csvFile:
+        reader = csv.reader(csvFile)
+        data = [row for row in reader]
+    return data
+
+
+def cleanData(data):
+    return list(filter(lambda thisList: False if '?' in thisList else True, data))
+
+
+def dummyVariables(features):
+    for column in range(features.shape[1]):
+        # 0,1,2,3,...,21
+        featureStatus = set(features[:, column])
+        tranasformer = preprocessing.LabelEncoder()
+        tranasformer.fit(list(featureStatus))
+        features[:, column] = tranasformer.transform(features[:, column])
+    return features
+
+# Read Data
+fileAddress = './train+dev+test.csv'
+data = readData(fileAddress)
+data = cleanData(data)
+data = np.array(data)[1:]  # remove headers
+
+# Seprate Feature and Lable
+X = data[:, 1:]
+Y = data[:, 0]
+X = dummyVariables(X)
+
+# split train and test subset I use 70% & 30%
+X_train, X_test, y_train, y_test = train_test_split(
+    X, Y, test_size=0.3, random_state=24)
+
+
